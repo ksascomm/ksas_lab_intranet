8b37ddf8ce712b71326909c3f83c5d5671a942f6
ksascomm <ksasweb@jhu.edu>
2017-11-06 11:30:07 -0500

    new social media links

--- footer.php
+++ footer.php
@@ -31,8 +31,10 @@
 		 ?>
 		<!-- Social Media -->
 		<nav class="two columns">
-			<a href="http://facebook.com/jhuksas" title="Facebook"><span class="fa fa-facebook-official fa-3x"></span><span class="screen-reader-text">Facebook</span></a>
-			<a href="https://www.youtube.com/user/jhuksas" title="YouTube"><span class="fa fa-youtube-square fa-3x"></span><span class="screen-reader-text">YouTube</span></a>
+			<a href="http://facebook.com/jhuksas" title="Facebook"><span class="fa fa-facebook-official fa-2x"></span></a>
+			<a href="https://www.instagram.com/jhuksas/" title="Instagram"><span class=" fa fa-instagram fa-2x"></span></a>
+			<a href="https://twitter.com/JHUKSAS" title="Twitter"><span class="fa fa-twitter fa-2x"></span></a>
+			<a href="https://www.youtube.com/user/jhuksas" title="YouTube"><span class="fa fa-youtube-square fa-2x"></span></a>
 		</nav>
 		
 		
